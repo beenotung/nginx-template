515c620d55f3b4f0e7d2471aff047c5f157f2a1e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-09-26 03:04:48 +0800

    init config: implemented basic cli to generate nginx .conf file for port forwarding

---
+++ core.ts
@@ -0,0 +1,125 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
+import { createInterface } from 'readline/promises'
+
+export async function main() {
+  let io = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  console.log(`
+nginx port forwarding .conf file generator
+
+## Simple example configuration
+port: 8100
+server_name: example.com
+
+## Multi host example configuration
+port: 8100
+server_name: www.example.com api.example.com
+
+Waiting inputs from cli...
+`)
+
+  var answer = await io.question('server_name: ')
+  let server_name = answer.trim().replaceAll(',', ' ')
+  if (!server_name) {
+    console.error('Invalid server_name')
+    process.exit(1)
+  }
+
+  let filename = server_name.split(' ')[0] + '.conf'
+
+  let content = readFromSystem(filename).trim()
+
+  let first = !content
+
+  if (content) {
+    content = updateConf(content)
+  } else {
+    var answer = await io.question('port: ')
+    let port = +answer
+    if (!port) {
+      console.error('Invalid port')
+      process.exit(1)
+    }
+    content = makeConf(server_name, port)
+  }
+
+  let dir = 'config.d'
+  mkdirSync(dir, { recursive: true })
+
+  let file = join(dir, filename)
+
+  writeFileSync(file, content.trim() + '\n')
+
+  if (first) {
+    console.log(`
+saved to: ${file}
+
+To continue run:
+$ sudo cp ${file} ${systemDir}/${filename}
+$ sudo nginx -t
+$ sudo certbot --nginx
+
+Then run this cli again to enable http2
+`)
+  } else {
+    console.log(`
+saved to: ${file}
+
+To continue run:
+$ sudo cp ${file} ${systemDir}/${filename}
+$ sudo nginx -t
+$ sudo service nginx restart
+`)
+  }
+  io.close()
+}
+
+let systemDir = '/etc/nginx/config.d'
+
+function readFromSystem(filename: string) {
+  try {
+    let file = `${systemDir}/${filename}`
+    return readFileSync(file).toString()
+  } catch (error) {
+    // file not exists
+    return ''
+  }
+}
+
+function updateConf(content: string): string {
+  return content
+    .split('\n')
+    .map(line => {
+      let from = 'listen 443 ssl'
+      let to = 'listen 443 ssl http2'
+      if (line.includes(from) && !line.includes(to)) {
+        return line.replace(from, to)
+      }
+      return line
+    })
+    .join('\n')
+}
+
+function makeConf(server_name: string, port: number): string {
+  return `
+server {
+  listen 80;
+  listen [::]:80;
+
+  server_name ${server_name};
+
+  location / {
+    proxy_pass http://localhost:${port};
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_cache_bypass $http_upgrade;
+  }
+}
+`
+}
